be626de03575b473507f36b22ab3392506feb158
Clay Baenziger <cbaenziger@bloomberg.net>
2014-08-13 00:38:12 +0000

    Working vagrant up and vagrant destroy for one disk or many (even with the same name, description & size) or if matching volumes already exist will attach to those

--- lib/vagrant-openstack-plugin/action/delete_server.rb
+++ lib/vagrant-openstack-plugin/action/delete_server.rb
@@ -1,10 +1,13 @@
 require "log4r"
+require 'vagrant/util/retryable'
+require 'timeout'
 
 module VagrantPlugins
   module OpenStack
     module Action
       # This deletes the running server, if there is one.
       class DeleteServer
+        include Vagrant::Util::Retryable
         def initialize(app, env)
           @app    = app
           @logger = Log4r::Logger.new("vagrant_openstack::action::delete_server")
@@ -15,17 +18,35 @@ module VagrantPlugins
           id = machine.id || env[:openstack_compute].servers.all( :name => machine.name ).first.id
 
           if id
+            volumes = env[:openstack_compute].servers.get(id).volume_attachments
+
             env[:ui].info(I18n.t("vagrant_openstack.deleting_server"))
 
             # TODO: Validate the fact that we get a server back from the API.
             server = env[:openstack_compute].servers.get(id)
             if server
               ip = server.floating_ip_address
-              server.destroy
               if machine.provider_config.floating_ip_pool
                 address = env[:openstack_compute].list_all_addresses.body["floating_ips"].find{|i| i["ip"] == ip}
                 if address
                   env[:openstack_compute].release_address(address["id"])
+              server.destroy
+              status = Timeout::timeout(90, Errors::ServerNotDestroyed) {
+                while server.reload
+                  sleep(1)
+                end
+              }
+
+              env[:ui].info(I18n.t("vagrant_openstack.deleting_volumes"))
+              volumes.each do |compute_volume|
+                volume = env[:openstack_volume].volumes.get(compute_volume["id"])
+                if volume
+                  env[:ui].info("Deleteing volume: #{volume.display_name}")
+                  begin
+                    volume.destroy
+                  rescue Excon::Errors::Error => e
+                    raise Errors::VolumeBadState, :volume => volume.display_name, :state => e.message
+                  end
                 end
               end
             end
